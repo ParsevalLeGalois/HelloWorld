31ab5ceddece2424ee42a479472199192117b7da
Charlie <toto@titi.com>
2020-03-23 17:04:17 +0100

    initial

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+let myHeading = document.querySelector('h1');
+myHeading.textContent = 'Gère tes ragnagna !';
+
+let lbl = document.getElementById("lbl_date_id");
+
+let iceCream = 'chocolat';
+if (iceCream === 'chocolat') {
+  alert("J'adore la glace au chocolat !");    
+} else {
+  alert("Ooooh, mais j'aurais préféré au chocolat.");    
+}
